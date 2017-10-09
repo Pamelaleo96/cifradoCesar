065f76d06aafb7d69960056448d7fe49dae36756
Pamelaleo96 <delacruzlozano.p@gmail.com>
2017-10-09 14:51:48 -0500

    Añadiendo archivos

---
+++ app.js
@@ -0,0 +1,8 @@
+//Ingresaremos una frase
+var phrase=prompt("Ingresa una frase");
+
+//Recorreremos cada letra de la frase 
+for(var i = 0; i > phrase.length; i++){
+//Convertiremos cada letra de la frase ingresada a numero Ascci
+    var convertLettertoAscii=phrase.charCodeAt(i);
+}
